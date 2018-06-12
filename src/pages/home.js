a36aee42f0ca7f6f266ea2a6d507aea80f7bd326
Trevor Blades <tdblades@gmail.com>
2018-06-12 02:12:35 -0700

    Add a homepage hero

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,38 @@
+import Button from '@material-ui/core/Button';
 import Helmet from 'react-helmet';
 import React, {Fragment} from 'react';
+import Typography from '@material-ui/core/Typography';
+import styled from 'react-emotion';
+import theme from '../theme';
+import {Link} from 'react-router-dom';
+
+const Hero = styled.div({
+  padding: theme.spacing.unit * 5,
+  textAlign: 'center'
+});
 
 const Home = () => (
   <Fragment>
     <Helmet>
       <title>Informing voters</title>
     </Helmet>
-    <div>Hello hey</div>
+    <Hero>
+      <Typography gutterBottom variant="display3">
+        Québec 2018
+      </Typography>
+      <Typography paragraph variant="subheading">
+        Our comprehensive election guide
+      </Typography>
+      <Button
+        size="large"
+        color="primary"
+        variant="raised"
+        component={Link}
+        to="/elections/quebec-2018"
+      >
+        Check it out
+      </Button>
+    </Hero>
   </Fragment>
 );
 
--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,10 @@
+import blue from '@material-ui/core/colors/blue';
 import blueGrey from '@material-ui/core/colors/blueGrey';
 import {createMuiTheme} from '@material-ui/core/styles';
 
 export default createMuiTheme({
   palette: {
+    primary: blue,
     grey: blueGrey
   },
   typography: {
