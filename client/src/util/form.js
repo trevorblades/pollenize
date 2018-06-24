f861c3136610bc0330834f2689c3a1978bebc72b
Trevor Blades <tdblades@gmail.com>
2018-06-23 21:44:31 -0700

    Remove unused util funcs

--- client/src/util/form.js
+++
@@ -1,5 +0,0 @@
-export function getSubmitButtonText(noun, id, loading) {
-  const verbs = [['Create', 'Creating'], ['Save', 'Saving']];
-  const verb = verbs[Number(Boolean(id))][Number(loading)];
-  return `${verb} ${noun}${loading ? '...' : ''}`;
-}
