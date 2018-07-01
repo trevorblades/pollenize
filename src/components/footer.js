c9522f1f0edf27d9c2a837e65b848b49d8b9a053
Trevor Blades <tdblades@gmail.com>
2018-06-30 23:56:39 -0700

    Add an email button to the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,4 @@
+import EmailIcon from '@material-ui/icons/Email';
 import Logo from '../assets/logo.svg';
 import Navigation from './navigation';
 import React from 'react';
@@ -6,6 +7,7 @@ import TwitterLogo from '../assets/logos/twitter.svg';
 import InstagramLogo from '../assets/logos/instagram.svg';
 import FacebookLogo from '../assets/logos/facebook.svg';
 import Typography from '@material-ui/core/Typography';
+import defaultProps from 'recompose/defaultProps';
 import styled, {css} from 'react-emotion';
 import theme from '../theme';
 import withProps from 'recompose/withProps';
@@ -38,16 +40,25 @@ const Icons = styled.div({
   display: 'flex'
 });
 
-const StyledAnchor = withProps({
+const StyledAnchor = styled.a({
+  ':not(:last-child)': {
+    marginRight: theme.spacing.unit
+  }
+});
+
+const BlankTargetAnchor = defaultProps({
   target: '_blank',
   rel: 'noopener noreferrer'
-})(
-  styled.a({
-    ':not(:last-child)': {
-      marginRight: theme.spacing.unit
-    },
-    svg: css(baseIconClassName, size(24))
-  })
+})(StyledAnchor);
+
+const logoSize = theme.spacing.unit * 3;
+const logoClassName = css(baseIconClassName, size(logoSize));
+
+const emailIconSize = theme.spacing.unit * 2.5;
+const StyledEmailIcon = styled(EmailIcon)(
+  baseIconClassName,
+  size(emailIconSize),
+  {margin: (logoSize - emailIconSize) / 2}
 );
 
 const date = new Date();
@@ -61,14 +72,17 @@ const Footer = () => (
           &copy; {year} Really Awesome Doings
         </Typography>
         <Icons>
-          <StyledAnchor href="https://twitter.com/pollenizeorg">
-            <TwitterLogo />
-          </StyledAnchor>
-          <StyledAnchor href="https://instagram.com/pollenize">
-            <InstagramLogo />
-          </StyledAnchor>
-          <StyledAnchor href="https://facebook.com/pollenize">
-            <FacebookLogo />
+          <BlankTargetAnchor href="https://twitter.com/pollenizeorg">
+            <TwitterLogo className={logoClassName} />
+          </BlankTargetAnchor>
+          <BlankTargetAnchor href="https://instagram.com/pollenize">
+            <InstagramLogo className={logoClassName} />
+          </BlankTargetAnchor>
+          <BlankTargetAnchor href="https://facebook.com/pollenize">
+            <FacebookLogo className={logoClassName} />
+          </BlankTargetAnchor>
+          <StyledAnchor href="mailto:info@pollenize.org">
+            <StyledEmailIcon />
           </StyledAnchor>
         </Icons>
       </div>
