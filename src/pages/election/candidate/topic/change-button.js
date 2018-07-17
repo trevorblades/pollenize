9622c05ad8c2d62d6169d643cb7668c15c297781
Trevor Blades <tdblades@gmail.com>
2018-07-17 15:00:47 -0700

    Add the candidate party to the change menu

--- src/pages/election/candidate/topic/change-button.js
+++ src/pages/election/candidate/topic/change-button.js
@@ -1,13 +1,16 @@
 import ButtonBase from '@material-ui/core/ButtonBase';
+import ListItemText from '@material-ui/core/ListItemText';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import PropTypes from 'prop-types';
 import React, {Component, Fragment} from 'react';
 import Typography from '@material-ui/core/Typography';
+import styled from 'react-emotion';
 import {connect} from 'react-redux';
-import {getLocalize} from '../../../../selectors';
+import {getLocalize, getMatchMessage} from '../../../../selectors';
 import {setCompareIndex} from '../../../../actions/settings';
 
+const StyledMenuItem = styled(MenuItem)({height: 'auto'});
 const menuOrigin = {
   horizontal: 'right',
   vertical: 'top'
@@ -18,7 +21,8 @@ class ChangeButton extends Component {
     comparates: PropTypes.array.isRequired,
     compareIndex: PropTypes.number.isRequired,
     dispatch: PropTypes.func.isRequired,
-    localize: PropTypes.func.isRequired
+    localize: PropTypes.func.isRequired,
+    matchMessage: PropTypes.func.isRequired
   };
 
   state = {
@@ -49,15 +53,18 @@ class ChangeButton extends Component {
           anchorEl={this.state.anchorEl}
           onClose={this.closeMenu}
         >
-          {this.props.comparates.map((comparate, index) => (
-            <MenuItem
-              key={comparate.id}
-              selected={index === this.props.compareIndex}
-              onClick={() => this.onMenuItemClick(index)}
-            >
-              {comparate.name}
-            </MenuItem>
-          ))}
+          {this.props.comparates.map((comparate, index) => {
+            const {message: party} = this.props.matchMessage(comparate.parties);
+            return (
+              <StyledMenuItem
+                key={comparate.id}
+                selected={index === this.props.compareIndex}
+                onClick={() => this.onMenuItemClick(index)}
+              >
+                <ListItemText primary={comparate.name} secondary={party.text} />
+              </StyledMenuItem>
+            );
+          })}
         </Menu>
       </Fragment>
     );
@@ -66,7 +73,8 @@ class ChangeButton extends Component {
 
 const mapStateToProps = state => ({
   compareIndex: state.settings.compareMode.index,
-  localize: getLocalize(state)
+  localize: getLocalize(state),
+  matchMessage: getMatchMessage(state)
 });
 
 export default connect(mapStateToProps)(ChangeButton);
