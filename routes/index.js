25fb24d0eb981908fb854093cbbef0b49c4b9dc2
Trevor Blades <tdblades@gmail.com>
2018-08-25 01:07:56 -0700

    Send back organizations for polly users

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import auth from './auth';
 import express from 'express';
 import candidates from './candidates';
 import elections from './elections';
+import organizations from './organizations';
 import positions from './positions';
 import topics from './topics';
 
@@ -10,6 +11,7 @@ router.get('/', (req, res) => res.sendStatus(200));
 router.use('/auth', auth);
 router.use('/candidates', candidates);
 router.use('/elections', elections);
+router.use('/organizations', organizations);
 router.use('/positions', positions);
 router.use('/topics', topics);
 
---
+++ routes/organizations.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import jwtMiddleware from '../middleware/jwt';
+import {Organization} from '../models';
+
+const router = express.Router();
+router.use(jwtMiddleware);
+
+router.get('/', async (req, res) => {
+  if (req.user.organization_id !== 1) {
+    res.sendStatus(403);
+    return;
+  }
+
+  const organizations = await Organization.findAll();
+  res.send(organizations);
+});
+
+export default router;
