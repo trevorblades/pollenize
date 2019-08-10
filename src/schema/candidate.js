e0a3577ac8f4027aa5cde67c683422053c4d296e
Trevor Blades <tdblades@gmail.com>
2019-08-10 01:01:26 -0700

    Add updateCandidate endpoint

--- src/schema/candidate.js
+++ src/schema/candidate.js
@@ -1,9 +1,23 @@
+import {AuthenticationError, UserInputError, gql} from 'apollo-server-express';
 import {Candidate} from '../db';
-import {gql} from 'apollo-server-express';
 
 export const typeDef = gql`
   extend type Query {
     candidate(id: ID!): Candidate
+  }
+
+  extend type Mutation {
+    updateCandidate(
+      id: ID!
+      name: String
+      partyEn: String
+      partyFr: String
+      birthDate: String
+      hometown: String
+      bioEn: String
+      bioFr: String
+      active: Boolean
+    ): Candidate
   }
 
   extend type Election {
@@ -34,6 +48,20 @@ export const resolvers = {
   Query: {
     candidate(parent, args) {
       return Candidate.findByPk(args.id);
+    }
+  },
+  Mutation: {
+    async updateCandidate(parent, {id, ...args}, {user}) {
+      if (!user) {
+        throw new AuthenticationError('Unauthorized');
+      }
+
+      const candidate = await Candidate.findByPk(id);
+      if (!candidate) {
+        throw new UserInputError('Candidate not found');
+      }
+
+      return candidate.update(args);
     }
   },
   Election: {
