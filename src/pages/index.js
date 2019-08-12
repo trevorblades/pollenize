146f60317ff300b4dacad6db41f184467a79eecc
Trevor Blades <tdblades@gmail.com>
2019-08-12 00:51:11 -0700

    Add quotes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,37 @@ import Footer from '../components/footer';
 import Header from '../components/header';
 import Hero from '../components/hero';
 import Layout from '../components/layout';
+import PropTypes from 'prop-types';
 import React from 'react';
-import {Box, Divider, Typography} from '@material-ui/core';
+import clouds from '../assets/images/clouds.jpg';
+import {Box, Grid, Typography} from '@material-ui/core';
 import {SectionWrapper} from '../components/common';
+import {styled} from '@material-ui/styles';
+
+const QuotesWrapper = styled('div')({
+  backgroundColor: '#7ebacf',
+  backgroundImage: `url(${clouds})`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center'
+});
+
+function Quote(props) {
+  return (
+    <Grid item xs={12} sm={6} md={4}>
+      <Box component="blockquote" m={0} mt={4} textAlign="center">
+        <Typography paragraph>&ldquo;{props.children}&rdquo;</Typography>
+        <Typography component="cite" variant="subtitle2">
+          &mdash;{props.cite}
+        </Typography>
+      </Box>
+    </Grid>
+  );
+}
+
+Quote.propTypes = {
+  cite: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired
+};
 
 export default function Index() {
   return (
@@ -15,7 +43,12 @@ export default function Index() {
         <Typography gutterBottom variant="h3">
           Informing voters
         </Typography>
-        <Box maxWidth={2 / 3}>
+        <Box
+          maxWidth={{
+            xs: 1,
+            md: 2 / 3
+          }}
+        >
           <Typography paragraph>
             Pollenize is a nonpartisan, apolitical non-profit organization
             seeking to provide voters with the most accurate election
@@ -32,7 +65,28 @@ export default function Index() {
           </Typography>
         </Box>
       </SectionWrapper>
-      <Divider />
+      <QuotesWrapper>
+        <SectionWrapper py={10}>
+          <Typography align="center" variant="h4">
+            We&apos;re creating a buzz 🐝
+          </Typography>
+          <Grid container spacing={8} justify="center">
+            <Quote cite="Matt Galloway, CBC">
+              You&apos;d be forgiven if you can&apos;t remember where each
+              [candidate] stands on the big issues. Pollenize is a site that is
+              trying to make it easier for voters.
+            </Quote>
+            <Quote cite="Leena Latafat, Global News">
+              Sifting through a pool of political information on the internet
+              can be overwhelming. But now, there&apos;s an app for that.
+            </Quote>
+            <Quote cite="Patricia D'Cunha, CityNews Toronto">
+              The Pollenize app has presented the candidates&apos; platforms in
+              a readable, friendly, and visually appealing format.
+            </Quote>
+          </Grid>
+        </SectionWrapper>
+      </QuotesWrapper>
       <Footer />
     </Layout>
   );
