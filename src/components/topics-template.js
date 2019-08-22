4f7e22f4b91c4a771df3f1098df5f7a0215986d5
Trevor Blades <tdblades@gmail.com>
2019-08-22 00:43:04 -0700

    Forgot to wrap topics template with a LanguageProvider

--- src/components/topics-template.js
+++ src/components/topics-template.js
@@ -10,11 +10,11 @@ import TopicWrapper from './topic-wrapper';
 import {Avatar, Box, Typography, styled} from '@material-ui/core';
 import {Link as GatsbyLink, graphql} from 'gatsby';
 import {Helmet} from 'react-helmet';
+import {LanguageProvider, useLocalize} from '../utils/language';
 import {Link} from 'gatsby-theme-material-ui';
 import {PageHeader, PageWrapper} from './common';
 import {getCandidateTitles, useCurrentAnchor} from '../utils';
 import {triangle} from 'polished';
-import {useLocalize} from '../utils/language';
 
 const triangleWidth = 24;
 const Triangle = styled(Box)(({theme}) =>
@@ -53,103 +53,112 @@ export default function TopicsTemplate(props) {
         <html lang={lang} />
         <title>{title}</title>
       </Helmet>
-      <HeaderBase link={electionPath} title={title}>
-        <ElectionMenu
-          title={title}
-          electionSlug={slug}
-          electionId={id}
-          candidates={candidates}
-          partyFirst={partyFirst}
-          intro={intro}
-          active="topics"
-          lang={lang}
-          languages={languages}
-          path={props.path}
-        />
-      </HeaderBase>
-      <PageHeader
-        title={localize('Topic explorer', 'Explorateur de sujets')}
-        subtitle={localize(
-          "View candidates' main stances organized by topic",
-          'Voir les positions principales des candidats organisées par sujet'
-        )}
-        bgcolor="grey.200"
-      />
-      <PageWrapper
-        sidebar={
-          <TableOfContents
-            topics={topics}
-            getActiveProps={index => ({
-              color: index === currentAnchor ? 'primary' : 'inherit'
-            })}
+      <LanguageProvider lang={lang} languages={languages} path={props.path}>
+        <HeaderBase link={electionPath} title={title}>
+          <ElectionMenu
+            title={title}
+            electionSlug={slug}
+            electionId={id}
+            candidates={candidates}
+            partyFirst={partyFirst}
+            intro={intro}
+            active="topics"
+            lang={lang}
+            languages={languages}
+            path={props.path}
           />
-        }
-      >
-        {topics.map((topic, index) => (
-          <TopicWrapper disableDivider={!index} key={topic.id} topic={topic}>
-            {topic.description && (
-              <Typography paragraph>{topic.description}</Typography>
-            )}
-            {topic.stances.map((stance, index) => {
-              const isOdd = Boolean(index % 2);
-              const pathToCandidate = `${electionPath}/${stance.candidate.slug}#${topic.slug}`;
-              const [title] = getCandidateTitles(stance.candidate, partyFirst);
+        </HeaderBase>
+        <PageHeader
+          title={localize('Topic explorer', 'Explorateur de sujets')}
+          subtitle={localize(
+            "View candidates' main stances organized by topic",
+            'Voir les positions principales des candidats organisées par sujet'
+          )}
+          bgcolor="grey.200"
+        />
+        <PageWrapper
+          sidebar={
+            <TableOfContents
+              topics={topics}
+              getActiveProps={index => ({
+                color: index === currentAnchor ? 'primary' : 'inherit'
+              })}
+            />
+          }
+        >
+          {topics.map((topic, index) => (
+            <TopicWrapper disableDivider={!index} key={topic.id} topic={topic}>
+              {topic.description && (
+                <Typography paragraph>{topic.description}</Typography>
+              )}
+              {topic.stances.map((stance, index) => {
+                const isOdd = Boolean(index % 2);
+                const pathToCandidate = `${electionPath}/${stance.candidate.slug}#${topic.slug}`;
+                const [title] = getCandidateTitles(
+                  stance.candidate,
+                  partyFirst
+                );
 
-              return (
-                <Box
-                  key={stance.id}
-                  maxWidth={{
-                    xs: 1,
-                    sm: 0.75
-                  }}
-                  display="flex"
-                  alignItems="flex-end"
-                  flexDirection={isOdd ? 'row-reverse' : 'row'}
-                  ml={isOdd ? 'auto' : 0}
-                  mt={index || topic.description || !topic.image ? 4 : 0}
-                >
-                  <Avatar
-                    component={GatsbyLink}
-                    src={stance.candidate.portrait}
-                    to={pathToCandidate}
-                  />
+                return (
                   <Box
-                    bgcolor="grey.200"
-                    borderRadius="borderRadius"
-                    position="relative"
-                    {...{
-                      [isOdd ? 'mr' : 'ml']: 2
+                    key={stance.id}
+                    maxWidth={{
+                      xs: 1,
+                      sm: 0.75
                     }}
+                    display="flex"
+                    alignItems="flex-end"
+                    flexDirection={isOdd ? 'row-reverse' : 'row'}
+                    ml={isOdd ? 'auto' : 0}
+                    mt={index || topic.description || !topic.image ? 4 : 0}
                   >
-                    <Triangle
-                      position="absolute"
-                      bottom={0}
-                      {...{
-                        [isOdd ? 'right' : 'left']: triangleWidth / -2
-                      }}
+                    <Avatar
+                      component={GatsbyLink}
+                      src={stance.candidate.portrait}
+                      to={pathToCandidate}
                     />
-                    <Box p={2}>
-                      <Typography gutterBottom>
-                        <StanceText
-                          stance={stance}
-                          sources={sources}
-                          onSourceClick={handleSourceClick}
-                        />
-                      </Typography>
-                      <Typography variant="caption" color="textSecondary">
-                        <Link color="inherit" to={pathToCandidate}>
-                          {title}
-                        </Link>
-                      </Typography>
+                    <Box
+                      bgcolor="grey.200"
+                      borderRadius="borderRadius"
+                      position="relative"
+                      {...{
+                        [isOdd ? 'mr' : 'ml']: 2
+                      }}
+                    >
+                      <Triangle
+                        position="absolute"
+                        bottom={0}
+                        {...{
+                          [isOdd ? 'right' : 'left']: triangleWidth / -2
+                        }}
+                      />
+                      <Box p={2}>
+                        <Typography gutterBottom>
+                          <StanceText
+                            stance={stance}
+                            sources={sources}
+                            onSourceClick={handleSourceClick}
+                          />
+                        </Typography>
+                        <Typography variant="caption" color="textSecondary">
+                          <Link color="inherit" to={pathToCandidate}>
+                            {title}
+                          </Link>
+                        </Typography>
+                      </Box>
                     </Box>
                   </Box>
-                </Box>
-              );
-            })}
-          </TopicWrapper>
-        ))}
-      </PageWrapper>
-      <Sources sources={sources} credits={credits} activeIndex={activeSource} />
+                );
+              })}
+            </TopicWrapper>
+          ))}
+        </PageWrapper>
+        <Sources
+          sources={sources}
+          credits={credits}
+          activeIndex={activeSource}
+        />
+      </LanguageProvider>
     </Layout>
   );
 }
