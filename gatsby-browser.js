a0fb1b5b6ae466e0590e30e802e413c662a6aa39
Trevor Blades <tdblades@gmail.com>
2019-08-24 13:59:30 -0700

    Swap browser/ssr responsibilities

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,10 @@
-export {wrapRootElement} from './gatsby-ssr';
+import React from 'react';
+import {StarsProvider} from './src/utils/stars';
+import {UserProvider} from './src/utils/user';
+
+// eslint-disable-next-line react/prop-types
+export const wrapRootElement = ({element}) => (
+  <StarsProvider>
+    <UserProvider>{element}</UserProvider>
+  </StarsProvider>
+);
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,1 @@
-import React from 'react';
-import {StarsProvider} from './src/utils/stars';
-import {UserProvider} from './src/utils/user';
-
-// eslint-disable-next-line react/prop-types
-export const wrapRootElement = ({element}) => (
-  <StarsProvider>
-    <UserProvider>{element}</UserProvider>
-  </StarsProvider>
-);
+export {wrapRootElement} from './gatsby-browser';
