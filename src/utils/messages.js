7f9accf7a439664684ccf1ed651187c603c8485d
Trevor Blades <tdblades@gmail.com>
2019-10-04 16:20:54 -0700

    Add polish client-side messages

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,56 +1,74 @@
 export default {
   About: {
-    fr: 'À propos de'
+    fr: 'À propos de',
+    pl: 'O'
   },
   'years old': {
-    fr: 'ans'
+    fr: 'ans',
+    pl: 'letni'
   },
   Hometown: {
-    fr: 'Ville natale'
+    fr: 'Ville natale',
+    pl: 'Miasto rodzinne'
   },
   'Show less': {
-    fr: 'Montre moins'
+    fr: 'Montre moins',
+    pl: 'Pokaż mniej'
   },
   'Show more': {
-    fr: 'Montre plus'
+    fr: 'Montre plus',
+    pl: 'Pokaż więcej'
   },
   Compare: {
-    fr: 'Comparer'
+    fr: 'Comparer',
+    pl: 'Porównać'
   },
   'More options': {
-    fr: "Plus d'options"
+    fr: "Plus d'options",
+    pl: 'Więcej opcji'
   },
   'Reset stars': {
-    fr: 'Réinitialiser les étoiles'
+    fr: 'Réinitialiser les étoiles',
+    pl: 'Zresetuj gwiazdki'
   },
   'Language: English': {
-    fr: 'Langue: Français'
+    fr: 'Langue: Français',
+    pl: 'Język: Polski'
   },
   'Open intro dialog': {
-    fr: "Ouvrir le dialogue d'introduction"
+    fr: "Ouvrir le dialogue d'introduction",
+    pl: 'Otwórz okno intro'
   },
   'Candidate grid': {
-    fr: 'Grille de candidats'
+    fr: 'Grille de candidats',
+    pl: 'Siatka dla kandydatów'
   },
   'Topic explorer': {
-    fr: 'Explorateur de sujets'
+    fr: 'Explorateur de sujets',
+    pl: 'Eksplorator tematów'
   },
   'Welcome to': {
-    fr: 'Bienvenue à'
+    fr: 'Bienvenue à',
+    pl: 'Witamy w'
   },
   'Change language': {
-    fr: 'Changer de langue'
+    fr: 'Changer de langue',
+    pl: 'Zmień język'
   },
   'Table of contents': {
-    fr: 'Table des matières'
+    fr: 'Table des matières',
+    pl: 'Spis treści'
   },
   "View candidates' main stances organized by topic": {
-    fr: 'Voir les positions principales des candidats organisées par sujet'
+    fr: 'Voir les positions principales des candidats organisées par sujet',
+    pl: 'Zobacz główne stanowiska kandydatów uporządkowane według tematów'
   },
   'No official stance has been taken on this topic.': {
-    fr: "Aucune position officielle n'a été prise sur ce sujet."
+    fr: "Aucune position officielle n'a été prise sur ce sujet.",
+    pl: 'Nie przyjęto oficjalnego stanowiska w tej sprawie.'
   },
   'Select your preferred language': {
-    fr: 'Sélectionnez votre langue préférée'
+    fr: 'Sélectionnez votre langue préférée',
+    pl: 'Wybierz preferowany język'
   }
 };
